6a1888812da6e2a3bc653a9d1b2e1eb5bff87c02
kh <kitt@cloudtogo.cn>
2017-06-15 12:28:03 +0800

    fix boot script

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ bazel build -c opt //im2txt:run_inference
 export DATASET=data.tgz
 export MODEL_POS=model
 curl -Lo ${DATASET} ${MODEL}
-[ -d ${MODEL_POS} ] && rm -rf ${MODEL_POS}
+[ ! -d ${MODEL_POS} ] && mkdir -p ${MODEL_POS}
 tar zxf ${DATASET} -C ${MODEL_POS}
 
 ./engine -ckeckpoint "${MODEL_POS}/train" -wordlist "${MODEL_POS}/data/word_counts.txt"
